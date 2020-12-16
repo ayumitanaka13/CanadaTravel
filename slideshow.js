f885c8d048e0dcceed2be10ff568884f8fd5b14d
wanko <kouji124@w4.dion.ne.jp>
2020-12-16 15:34:13 -0800

    still editing

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,29 @@
+const mediaQuery = window.matchMedia('(min-width: 425px)');
+
 var swiper = new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true
+
+    // if(matchMedia('(min-width: 425px)').matches) {
+      slidesPerView: 1,
+      breakpoints: {
+        // 425px以上の場合
+        425: {
+          slidesPerView: 2,
+        },
+        // 980px以上の場合
+        980: {
+          slidesPerView: 3,
+        },
+      },
+      spaceBetween: 30,
+      loop: true,
+    // }
+
+
+
+
 
   });
